d4538e915667c46b2f29330aea1e1f777e303809
Ldoun <67096173+Ldoun@users.noreply.github.com>
2021-06-03 15:45:44 +0900

    feat: develop train.py

--- train.py
+++ train.py
@@ -4,11 +4,11 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch_optimizer as custom_optim
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 
 from alignment.trainer import SingleTrainer,MaximumLikelihoodEstimationEngine
 from alignment.dataloader import LJSpeechDataset,RandomBucketBatchSampler,TextAudioCollate
-import alignment.Tokenizer as tokenizer
+from alignment.Tokenizer import tokenizer
 
 from alignment.model.lyrics_alignment import alignment_model
 
@@ -28,20 +28,11 @@ def define_argparser(is_continue=False):
         required=not is_continue,
         help='Model file name to save. Additional information would be annotated to the file name.'
     )
+
     p.add_argument(
-        '--train',
+        '--tsv',
         required=not is_continue,
-        help='Training set file name except the extention. (ex: train.en --> train)'
-    )
-    p.add_argument(
-        '--valid',
-        required=not is_continue,
-        help='Validation set file name except the extention. (ex: valid.en --> valid)'
-    )
-    p.add_argument(
-        '--test',
-        required=not is_continue,
-        help='test set file name except the extention. (ex: test.en --> test)'
+        help='Training set file name'
     )
     
     p.add_argument(
@@ -51,15 +42,17 @@ def define_argparser(is_continue=False):
     )
 
     p.add_argument(
-        '--tsv',
-        required=not is_continue ,
-        help = 'tsv file path'
+        '--train_size',
+        type=int,
+        default=2000,
+        help='length of train set'
     )
-
+    
     p.add_argument(
-        '--vocab_f',
-        required=not is_continue ,
-        help = 'vocab pickle file path'
+        '--valid_size',
+        type=int,
+        default=838,
+        help='length of valid set'
     )
 
     p.add_argument(
@@ -102,17 +95,24 @@ def define_argparser(is_continue=False):
     )
 
     p.add_argument(
-        '--max_length',
-        type=int,
-        default=100,
-        help='Maximum length of the training sequence. Default=%(default)s'
+        '--dropout',
+        type=float,
+        default=.1,
+        help='Dropout rate. Default=%(default)s'
     )
 
     p.add_argument(
-        '--dropout',
-        type=float,
-        default=.2,
-        help='Dropout rate. Default=%(default)s'
+        '--tbtt_step',
+        type=int,
+        default=40,
+        help='tbtt_step. Default=%(default)s'
+    )
+
+    p.add_argument(
+        '--W',
+        type=int,
+        default=55,
+        help='W. Default=%(default)s'
     )
 
     p.add_argument(
@@ -123,17 +123,31 @@ def define_argparser(is_continue=False):
     )
 
     p.add_argument(
-        '--hidden_size',
+        '--en_hs',
         type=int,
-        default=768,
-        help='Hidden size of LSTM. Default=%(default)s'
+        default=512,
+        help='encoder Hidden size'
     )
     
     p.add_argument(
-        '--n_layers',
+        '--de_hs',
         type=int,
-        default=4,
-        help='Number of layers in LSTM. Default=%(default)s'
+        default=1024,
+        help='decoder Hidden size'
+    )
+
+    p.add_argument(
+        '--attention_dim',
+        type=int,
+        default=256,
+        help='attention dim size'
+    )
+
+    p.add_argument(
+        '--location_feature_dim',
+        type=int,
+        default=128,
+        help='location_feature dim size'
     )
 
     p.add_argument(
@@ -148,17 +162,11 @@ def define_argparser(is_continue=False):
         action='store_true',
         help='Use Adam as optimizer instead of SGD. Other lr arguments should be changed.',
     )
-    p.add_argument(
-        '--use_radam',
-        action='store_true',
-        help='Use rectified Adam as optimizer. Other lr arguments should be changed.',
-    )
 
     p.add_argument(
         '--log_dir',
         type=str,
-        required=False,
-        default='tensorboard_logs'
+        default='../tensorboard'
     )
 
     config = p.parse_args()
@@ -168,11 +176,13 @@ def define_argparser(is_continue=False):
 def get_model(input_size, output_size, config):
     model = alignment_model(
             input_size,
-            config.word_vec_size,           # Word embedding vector size
-            config.hidden_size,             # LSTM's hidden vector size
             output_size,
-            n_layers=config.n_layers,       # number of layers in LSTM
-            dropout_p=config.dropout        # dropout-rate in LSTM
+            config.word_vec_size,  
+            config.en_hs,
+            config.de_hs,
+            config.attention_dim,
+            config.location_feature_dim,
+            config.dropout 
         )
 
     return model
@@ -193,45 +203,38 @@ def get_crit(output_size, pad_index):
 
 def get_optimizer(model, config):
     if config.use_adam:
-        if config.use_transformer:
-            optimizer = optim.Adam(model.parameters(), lr=config.lr, betas=(.9, .98))
-        else: # case of rnn based seq2seq.
-            optimizer = optim.Adam(model.parameters(), lr=config.lr)
-    elif config.use_radam:
-        optimizer = custom_optim.RAdam(model.parameters(), lr=config.lr)
-    else:
-        optimizer = optim.SGD(model.parameters(), lr=config.lr)
+        optimizer = optim.Adam(model.parameters(), lr=config.lr,weight_decay=1e-6)
 
     return optimizer
 
-def main(config, model_weight=None, opt_weight=None):
+def main(config, model_weight=None, opt_weight=None, vocab = None):
     def print_config(config):
         pp = pprint.PrettyPrinter(indent=4)
         pp.pprint(vars(config))
     print_config(config)
 
-    tok = tokenizer(config.vocab_f)
-    dataset = LJSpeechDataset(config.music_dir,config.tsv,tok = tok)
-    batch_sampler = RandomBucketBatchSampler(dataset, batch_size=5, drop_last=False)
+    tok = tokenizer(vocab)
+    if vocab is None:
+        tok.set_vocab(config.tsv)
+
+    dataset = LJSpeechDataset(config.music_dir,config.tsv,tok = tok )
+
+    train_dataset,valid_dataset = random_split(dataset,[config.train_size,config.valid_size],generator=torch.Generator().manual_seed(42))
+    
+    train_batch_sampler = RandomBucketBatchSampler(train_dataset, batch_size=config.batch_size, drop_last=False)
+    valid_batch_sampler = RandomBucketBatchSampler(valid_dataset, batch_size=config.batch_size, drop_last=False)
+    
     collate_fn = TextAudioCollate()
-    dataloader = DataLoader(dataset, batch_sampler=batch_sampler,
-                            collate_fn=collate_fn)
 
-    tok.save(config.vocab_f)
+    train_dataloader = DataLoader(train_dataset, batch_sampler=train_batch_sampler,collate_fn=collate_fn)
+    valid_dataloader = DataLoader(valid_dataset, batch_sampler=valid_batch_sampler,collate_fn=collate_fn)
 
-    loader = DataLoader(
-        config.train,                           # Train file name except extention, which is language.
-        config.valid,                           # Validation file name except extension.
-        (config.lang[:2], config.lang[-3:]),    # Source and target language.
-        batch_size=config.batch_size,
-        device=-1,                              # Lazy loading
-        max_length=config.max_length,           # Loger sequence will be excluded.
-        dsl=False,                              # Turn-off Dual-supervised Learning mode.
-    )
+    print(tok.vocab)
+    print('-' * 80)
 
-    input_size, output_size = len(loader.src.vocab), len(loader.tgt.vocab)
+    input_size, output_size = 128, len(tok.vocab)
     model = get_model(input_size, output_size, config)
-    crit = get_crit(output_size, data_loader.PAD)
+    crit = get_crit(output_size, tok.pad)
 
     if model_weight is not None:
         model.load_state_dict(model_weight)
@@ -260,16 +263,14 @@ def main(config, model_weight=None, opt_weight=None):
         model,
         crit,
         optimizer,
-        train_loader=loader.train_iter,
-        valid_loader=loader.valid_iter,
-        src_vocab=loader.src.vocab,
-        tgt_vocab=loader.tgt.vocab,
+        train_loader=train_dataloader,
+        valid_loader=valid_dataloader,
+        vocab=tok.vocab,
         n_epochs=config.n_epochs,
     )
 
     mle_trainer.tb_logger.close()
 
-
 if __name__ == '__main__':
     config = define_argparser()
-    main(config)
+    main(config)    
