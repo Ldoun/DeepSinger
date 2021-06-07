79b6967d58d0561c08e723a7530277548bca962a
Ldoun <67096173+Ldoun@users.noreply.github.com>
2021-06-08 08:00:43 +0900

    test:nan loss

--- alignment/model/lyrics_alignment.py
+++ alignment/model/lyrics_alignment.py
@@ -132,6 +132,7 @@ class ConvolutionBlock(nn.Module):
 
     def forward(self,x):
         for row in x:
+            print(torch.sum(row,dim=-1))
             if torch.sum(row,dim=-1) == 0:
                 print('yes...')
         #print('input_x',torch.isnan(x).any())
