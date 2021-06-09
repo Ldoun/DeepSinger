793e4aa022380c294f47723acf7b61755055ba04
Ldoun <67096173+Ldoun@users.noreply.github.com>
2021-06-09 14:39:48 +0900

    fix: undo drop_last True

--- train.py
+++ train.py
@@ -235,7 +235,7 @@ def main(config, model_weight=None, opt_weight=None, vocab = None):
 
     train_dataset,valid_dataset = random_split(dataset,[config.train_size,config.valid_size],generator=torch.Generator().manual_seed(42))
     
-    train_batch_sampler = RandomBucketBatchSampler(train_dataset, batch_size=config.batch_size, drop_last=False)
+    train_batch_sampler = RandomBucketBatchSampler(train_dataset, batch_size=config.batch_size, drop_last=True)
     valid_batch_sampler = RandomBucketBatchSampler(valid_dataset, batch_size=config.valid_batch_size, drop_last=True)
     
     collate_fn = TextAudioCollate()
