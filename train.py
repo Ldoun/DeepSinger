3f6de5c9e13802a774286d258b7e89e0ac0b2cf2
Ldoun <67096173+Ldoun@users.noreply.github.com>
2021-06-10 15:56:29 +0900

    feat: add graph

--- train.py
+++ train.py
@@ -227,7 +227,7 @@ def add_graph(model,tb_logger,dataloader):
     data = iter(dataloader).next()
     device = next(model.parameters()).device
     x,mask,x_length = data[0][0].to(device),data[0][1].to(device),data[0][2] #tensor,mask,length
-    y,_ = (data[1][0][:,:-1],data[1][1])
+    y,_ = (data[1][0][:,:-1].to(device),data[1][1])
     tb_logger.writer.add_graph(model=model,input_to_model=((x,mask),y) ,verbose=True)
 
 
