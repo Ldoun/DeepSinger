f248a0e7fe6426c59ef43f407841b8ea44192c29
Ldoun <67096173+Ldoun@users.noreply.github.com>
2021-06-14 09:08:17 +0900

    fix: load model for validation

--- alignment/trainer.py
+++ alignment/trainer.py
@@ -353,7 +353,7 @@ class MaximumLikelihoodEstimationEngine(Engine):
 
     @staticmethod
     def load_model_for_val(engin,train_engin):
-        engin.load_state_dict(train_engin.model.state_dict())
+        engin.model.load_state_dict(train_engin.model.state_dict())
         print('loded')
         
     @staticmethod
