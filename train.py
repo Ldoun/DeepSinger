ee8068499eaed88a0bfc106351b9ef33e8a34cab
Ldoun <67096173+Ldoun@users.noreply.github.com>
2021-06-21 11:49:34 +0900

    fix: drop last

--- train.py
+++ train.py
@@ -316,8 +316,8 @@ def main(config, model_weight=None, opt_weight=None):
 
     #train_dataset,valid_dataset = random_split(dataset,[config.train_size,config.valid_size]) #,generator=torch.Generator().manual_seed(42)'''
     
-    train_batch_sampler = RandomBucketBatchSampler(train_dataset, batch_size=config.batch_size, drop_last=True)
-    valid_batch_sampler = RandomBucketBatchSampler(valid_dataset, batch_size=config.batch_size, drop_last=True)
+    train_batch_sampler = RandomBucketBatchSampler(train_dataset, batch_size=config.batch_size, drop_last=False)
+    valid_batch_sampler = RandomBucketBatchSampler(valid_dataset, batch_size=config.batch_size, drop_last=False)
     
     collate_fn = TextAudioCollate()
 
