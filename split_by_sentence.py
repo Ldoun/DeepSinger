3babfe88b59ea2b5735d336cb5cad58342b99d41
Ldoun <67096173+Ldoun@users.noreply.github.com>
2021-06-26 17:23:58 +0900

    fix: dataframe slicing

--- split_by_sentence.py
+++ split_by_sentence.py
@@ -127,8 +127,8 @@ if __name__ == '__main__':
     with torch.no_grad():
         device = next(model.parameters()).device
         for input_data in data.iterrows():
-            x = load_audio(os.path.join(config.music_dir,input_data[1][1]['video_name'])).unsqueeze(0)
-            y = np.array(tok.get_idx(input_data[1][1]['lyrics']))
+            x = load_audio(os.path.join(config.music_dir,input_data[1]['video_name'])).unsqueeze(0)
+            y = np.array(tok.get_idx(input_data[1]['lyrics']))
 
             input_y = torch.IntTensor(y)[:-1].unsqueeze(0)
             x_length = x.size(2)
