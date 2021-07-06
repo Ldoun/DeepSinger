18fb732be0bd21ab6fa833235ee0dc46ae19baf6
Ldoun <67096173+Ldoun@users.noreply.github.com>
2021-07-06 09:02:15 +0900

    feat: preprocess

---
+++ data_crawling/preprocess.py
@@ -0,0 +1,70 @@
+import sys
+from scipy.io.wavfile import write
+import os
+import librosa
+import math
+import numpy as np
+
+import pyloudnorm as pyln
+import parselmouth
+from spleeter.audio.adapter import AudioAdapter
+from spleeter.separator import Separator
+from spleeter.audio.adapter import AudioAdapter
+
+music_dir = sys.argv[1]
+output_dir = sys.argv[2]
+
+audio_loader = AudioAdapter.default()
+separator = Separator('spleeter:2stems')
+
+thresh_hold = 0.03
+files = os.listdir(music_dir)
+for index,m_file in enumerate(files):
+    print(len(files) - index)
+    if os.path.isfile(os.path.join(output_dir,m_file)):
+        continue
+
+    #vocal extraction
+    waveform, _ = audio_loader.load(os.path.join(music_dir,m_file), sample_rate=22050)
+    prediction = separator.separate(waveform)
+
+    y = np.swapaxes(prediction['vocals'], 0, 1)
+    y = librosa.to_mono(y)
+
+    meter = pyln.Meter(22050) 
+    loudness = meter.integrated_loudness(y)
+
+    #normalize loudness
+    loudness_normalized_audio = pyln.normalize.loudness(y, loudness, -16.0)
+
+    snd = parselmouth.Sound(loudness_normalized_audio)
+    pitch = snd.to_pitch()
+    pitch_values = pitch.selected_array['frequency']
+    
+
+    fitered_voice = []
+    non_vocal_frame_cnt = 0
+    frames_sum = []
+    if pitch_values[0] ==0 or sum(abs(loudness_normalized_audio[:441])) <= 441*thresh_hold:
+        non_vocal_frame_cnt += 1
+    else:
+        fitered_voice = fitered_voice + loudness_normalized_audio[:441]
+
+    for i,p in enumerate(pitch_values):
+        index = i + 1 
+        frame_cnt = 441 + (index % 2)
+        frame_sum = sum(abs(loudness_normalized_audio[math.floor(441 * index):math.floor(441 * index) + frame_cnt]))
+        frames_sum.append(frame_sum)
+        if p == 0 or frame_sum < frame_cnt*thresh_hold:
+            non_vocal_frame_cnt += 1
+        
+        else:
+            if non_vocal_frame_cnt > 10:
+                for i in range(4410):
+                    fitered_voice.append(0)
+                
+            fitered_voice = fitered_voice + list(loudness_normalized_audio[math.floor(441 * index):math.floor(441 * index) + frame_cnt])
+            non_vocal_frame_cnt = 0
+
+    fitered_voice = np.array(fitered_voice)
+    write(os.path.join(output_dir,m_file),22050,fitered_voice)
